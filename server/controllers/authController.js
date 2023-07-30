80e4b9276487a261ec34cdc71b15ad3391793fd5
ZacharySal <zach.salvaggio@gmail.com>
2023-07-29 19:53:44 -0500

    bcrypt fix

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -1,6 +1,6 @@
 const User = require("../models/User");
 
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const asyncHandler = require("express-async-handler");
 require("dotenv").config();
